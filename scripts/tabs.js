93ac159ab6e24895a06c6cfc716c16112cdd7c8a
kotek900 <kotek@kotek900.com>
2024-05-15 19:34:20 +0200

    added tabs.js

---
+++ scripts/tabs.js
@@ -0,0 +1,32 @@
+window.addEventListener("DOMContentLoaded", () => {
+  const tabs = document.querySelectorAll('[role="tab"]');
+  const tabList = document.querySelector('[role="tablist"]');
+
+  tabs.forEach((tab) => {
+    tab.addEventListener("click", changeTabs);
+  });
+});
+
+function changeTabs(e) {
+  const target = e.target;
+  const parent = target.parentNode.parentNode;
+  const grandparent = parent.parentNode.parentNode;
+
+  // Remove all current selected tabs
+  parent
+    .querySelectorAll('[aria-selected="true"]')
+    .forEach((t) => t.setAttribute("aria-selected", false));
+
+  // Set this tab as selected
+  target.setAttribute("aria-selected", true);
+
+  // Hide all tab panels
+  grandparent
+    .querySelectorAll('[role="tabpanel"]')
+    .forEach((p) => p.setAttribute("hidden", true));
+
+  // Show the selected panel
+  grandparent.parentNode
+    .querySelector(`#${target.getAttribute("aria-controls")}`)
+    .removeAttribute("hidden");
+}
